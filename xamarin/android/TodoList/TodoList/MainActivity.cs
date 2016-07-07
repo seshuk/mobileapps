b57415b78781319100e09ea603a29944a63c6659
Seshu Koorella <k.seshu@live.in>
2016-07-07 16:47:37 +1000

    Initial commit. Demo app.

---
+++ xamarin/android/TodoList/TodoList/MainActivity.cs
@@ -0,0 +1,65 @@
+﻿using Android.App;
+using Android.Views;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Widget;
+using Toolbar = Android.Support.V7.Widget.Toolbar;
+using com.refractored.fab;
+
+namespace TodoList
+{
+    [Activity(Label = "TodoList", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : AppCompatActivity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+
+            //Toolbar will now take on default actionbar characteristics
+            SetSupportActionBar(toolbar);
+
+            SupportActionBar.Title = "List of ToDos";
+
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+
+            var items = new string[] { "Vegetables", "Fruits", "Flower Buds", "Legumes", "Bulbs", "Tubers", "Vegetables", "Fruits", "Flower Buds", "Legumes", "Bulbs", "Tubers" };
+
+            var listView = FindViewById<ListView>(Resource.Id.listView1);
+
+            listView.Adapter = new ArrayAdapter(this, Resource.Layout.ListViewTemplate, items);
+            var fab = FindViewById<FloatingActionButton>(Resource.Id.faButton);
+            fab.AttachToListView(listView);
+
+        }
+
+        /// <Docs>The options menu in which you place your items.</Docs>
+		/// <returns>To be added.</returns>
+		/// <summary>
+		/// This is the menu for the Toolbar/Action Bar to use
+		/// </summary>
+		/// <param name="menu">Menu.</param>
+		public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.home, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            Toast.MakeText(this, "Top ActionBar pressed: " + item.TitleFormatted, ToastLength.Short).Show();
+            return base.OnOptionsItemSelected(item);
+        }
+    }
+}
+
