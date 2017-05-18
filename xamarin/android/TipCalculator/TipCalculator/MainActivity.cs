6c985b451e5021febe8fea5fb4fe408c6d4809e3
Seshu Koorella <k.seshu@live.in>
2017-05-18 16:56:12 +1000

    Added iOS and Android projects

---
+++ xamarin/android/TipCalculator/TipCalculator/MainActivity.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace TipCalculator
+{
+    [Activity(Label = "TipCalculator", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        EditText inputBill;
+        Button calcButton;
+        TextView outputTip;
+        TextView outputTotal;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            inputBill = FindViewById<EditText>(Resource.Id.inputBill);
+            calcButton = FindViewById<Button>(Resource.Id.calcButton);
+            outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
+
+            calcButton.Click += (sender, e) => {
+                var billAmt = inputBill.Text;
+                var bill = double.Parse(billAmt);
+
+                var tip = bill * 0.15;
+                var total = bill + tip;
+
+                outputTip.Text = tip.ToString();
+                outputTotal.Text = total.ToString();
+
+            };
+        }
+    }
+}
+
---
+++ xamarin/ios/xam_u/TipCalculator/MyViewController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using CoreGraphics;
+using UIKit;
+
+namespace TipCalculator
+{
+    public class MyViewController : UIViewController
+    {
+        public MyViewController()
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            
+            base.ViewDidLoad();
+            this.View.BackgroundColor = UIColor.Yellow;
+
+            UITextField TotalAmount = new UITextField(new CGRect(28, 28, this.View.Bounds.Width - 40, 35)){
+                KeyboardType = UIKeyboardType.NumberPad ,
+                BorderStyle = UITextBorderStyle.RoundedRect,
+                Placeholder = "Enter an amount"
+            };
+
+            UIButton calc = new UIButton(UIButtonType.Custom) { 
+                Frame = new CGRect(20, 71, View.Bounds.Width-40, 35 ),
+                BackgroundColor = UIColor.FromRGB(0, 0.5f, 0)
+            };
+
+            calc.SetTitle("Calculate", UIControlState.Normal);
+
+            UILabel resultLabel = new UILabel(new CGRect(0, 124, View.Bounds.Width, 40)) {
+                TextColor = UIColor.Blue,
+                TextAlignment = UITextAlignment.Center,
+                Font = UIFont.SystemFontOfSize(24),
+                Text = "Tip is $0.00"
+            };
+
+            View.AddSubviews(TotalAmount, calc, resultLabel);
+
+            calc.TouchUpInside += (s, e) => {
+                Double val = 0;
+                Double.TryParse(TotalAmount.Text, out val);
+
+
+                resultLabel.Text = string.Format("Tip is ${0}", val * 0.2);
+
+                TotalAmount.ResignFirstResponder();
+            };
+        }
+
+    }
+}
